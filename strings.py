cbd296d7f20dfaa019dc454578ba7b716a9070ca
GayathriBinu <104920263+GayathriBinu@users.noreply.github.com>
2022-05-10 12:43:38 -0400

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,30 @@
+""" 
+Strings are datatype.
+Strings can be changed.
+Strings are written within single or double quotation marks.
+All user input are strings. 
+
+""" 
+
+string1 = "This is a string."
+
+#string2 = 'This is also a string.'
+#print(string1)
+
+print(string1[1])
+print(string1[5:10])
+print(string1[:6])
+print(string1[3:])
+print(string1[:])
+print(string1 * 2)
+
+var2 = ord('C')
+print(var2)
+
+var3 = chr(0)
+print(var3)
+
+#print(string2)
+#var1 = input("Please enter some text:")
+#print("You entered:" + var1)
+
