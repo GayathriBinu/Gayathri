dc42ac50d3036d12324c5924f20618326a730bba
GayathriBinu <104920263+GayathriBinu@users.noreply.github.com>
2022-05-10 12:40:03 -0400

    Create ifStatement.py

---
+++ ifStatement.py
@@ -0,0 +1,13 @@
+print("type a number less than 10")
+
+user_number = int(input())
+
+if user_number < 10:
+    print(user_number)
+   
+    print("Please go to the next step")
+    print("Have a nice day!")
+    
+if user_number > 10:
+    print("incorrect number, please try again.")
+    
