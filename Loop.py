3ea6e5f3028e89ecaf7ba0d94edb21456a8a9911
GayathriBinu <104920263+GayathriBinu@users.noreply.github.com>
2022-05-10 12:41:48 -0400

    Create Loop.py

---
+++ Loop.py
@@ -0,0 +1,24 @@
+"""
+for counter in range(3,10,2):
+    print(counter)
+    print("Hello")
+    
+    
+myName = "Gayathri"
+
+for letter in myName: 
+    print(letter)      
+
+number = 1
+while number < 10:
+    print("Hello")
+    number = number + 1 
+"""
+
+count = 0 
+userInput = int(input("Please enter a number:"))
+
+while count < userInput:
+    print("Hello World!")
+    #count += 1
+    count = count + 1
