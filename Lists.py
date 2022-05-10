acf2bc1d03f0516b35f92a86db0a9f1415954b7c
GayathriBinu <104920263+GayathriBinu@users.noreply.github.com>
2022-05-10 12:41:07 -0400

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,8 @@
+gayathriList = [] # creating empty list 
+
+gayathriList2 = [1, "foo", 3.3, 'x'] #creating list with elements
+
+print(gayathriList) #prints entire list
+print(gayathriList2) #prints entire list
+print(gayathriList2[2]) # prints 3rd element in list
+print(gayathriList2[1:3]) # prints a range of elements 
