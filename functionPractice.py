cc58c41c2ceb82928df4056033e594dbcfece231
GayathriBinu <104920263+GayathriBinu@users.noreply.github.com>
2022-05-19 11:49:44 -0400

    Create functionPractice.py

---
+++ functionPractice.py
@@ -0,0 +1,13 @@
+# Examples of functions 
+
+print('Please enter a number: ')
+
+variable = input()
+
+print(variable)
+
+variable = int(variable)
+variable = float(variable)
+variable = str(variable)
+
+print('You entered ' + variable)
